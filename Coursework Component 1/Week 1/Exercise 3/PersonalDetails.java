3330076396c18d888d84c7504002298ed96b7a91
Yuri Roka <yuriroka86@gmail.com>
2025-02-27 19:19:50 +0545

    Add files via upload

---
+++ Coursework Component 1/Week 1/Exercise 3/PersonalDetails.java
@@ -0,0 +1,13 @@
+package Java_Coursework;
+import java.util.Scanner;
+
+public class PersonalDetails {
+	public static void main(String args[]) {
+		String name = "Yuri Roka";
+		int age = 19;
+		double salary = 450000;
+		int number = 912345678;
+		System.out.println("Name: " + name + "\nAge: " + age + "\nSalary: " + salary + "\nPhone Number: " + number);
+	}
+
+}
