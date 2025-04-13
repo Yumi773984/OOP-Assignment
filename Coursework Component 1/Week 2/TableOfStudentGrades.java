a5c3a33c22c1ee3c138073a10c41a581af116284
Yuri Roka <yuriroka86@gmail.com>
2025-04-13 20:36:10 +0545

    Add files via upload

---
+++ Coursework Component 1/Week 2/TableOfStudentGrades.java
@@ -0,0 +1,20 @@
+package Java_Coursework;
+
+public class TableOfStudentGrades {
+
+	public static void main(String[] args) {
+		System.out.println("///////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("==          Student Points          ==");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\///////////////////\n");
+
+        System.out.println("Name\t\tLab\tBonus\tTotal");
+        System.out.println("----\t\t---\t-----\t-----");
+        System.out.println("Yuri\t\t45\t5\t" + (45 + 5));
+        System.out.println("Smirti\t\t40\t8\t" + (40 + 8));
+        System.out.println("Bhawana\t\t48\t7\t" + (48 + 7));
+        System.out.println("Ram\t\t42\t6\t" + (42 + 6));
+        System.out.println("Shyam\t\t39\t9\t" + (39 + 9));
+    }
+
+}
+
